a2ce9ce0b0b57ea491bf0c29f46eeba886fa589b
Aditi Patil <aditipatil0506@gmail.com>
2024-03-20 14:15:50 +0530

    initial commit

--- src/userRegistration.java
+++ src/userRegistration.java
@@ -3,23 +3,22 @@ import java.util.regex.*;
 
 class main{
     public static void main(String args[]){
-        Scanner scanner = new Scanner (System.in);
-        System.out.println("Enter the Last Name:");
-        String lastName = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Email Id:");
+        String emailId = scanner.nextLine();
 
-        if(isValidLastName(lastName)){
-            System.out.println("Valid Last Name:");
-        }else {
-            System.out.println("Invalid Last Name:");
+        if(isValidEmailId (emailId)){
+            System.out.println("Valid Email Id:");
+        } else{
+            System.out.println("Invalid Email Id:");
         }
     }
 
-    public static boolean isValidLastName( String lastName){
-        String Regex = "[A-Z]{1}[a-z ]{2,}";
+    public static boolean isValidEmailId(String emailId){
+        String Regex = "[a-z 0-9]+[@]+[a-z]+[.]+[a-z]{2,3}";
         Pattern pattern = Pattern.compile(Regex);
-        Matcher matcher = pattern.matcher(lastName);
+        Matcher matcher = pattern.matcher(emailId);
 
         return matcher.matches();
-
     }
-}
+        }
